3c58f1aa01abf6e4e5514f15c118869ded6f6efc
Mahdi-Ostadrahim <mahdi.ostadrahim@gmail.com>
2022-10-01 07:40:01 +0330

    .

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html',context)
 
-def blog_single(request):
-    context = {'title':'bitcoin crashed again!','content':'bitcoin was flyinh but now grounded as always!','author':'MahdiOR'}
+def blog_single(request,pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(posts, pk=pid)
+    context = {'post': post}
     return render(request, 'blog/blog-single.html',context)
+
+def test(request,pid):
+    #post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post': post}
+    return render(request,'test.html',context)
